43df3bc8cd5197815ba274d235ab01a323abeb9e
sazare <song.of.sand@gmail.com>
2019-02-06 20:30:21 +0900

    parsej parse the statement. grammatical words are romaji, others are Japanese. 	new file:   jugon.jl

---
+++ jugon.jl
@@ -0,0 +1,62 @@
+function parsej(m::String)
+ token = []
+ rjo = r"(.+)(wo|ha|ni|ru)"
+ pm  = m
+ tks = ""
+ while true
+   tks = match(rjo, pm)
+   if nothing == tks; break end
+   cm = tks.captures
+   push!(token, (cm[1], cm[2]))
+   pm = cm[1]
+ end
+ return token
+end
+
+#==
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
+julia> match(r"(.+)ha","わたしha")
+RegexMatch("わたしha", 1="わたし")
+
+julia> match(r"([a-z]*)(.+)wo",m3)
+RegexMatch("君ha僕niケーキwo", 1="", 2="君ha僕niケーキ")
+
+julia> match(r"([a-z]+)(.+)wo",m3)
+RegexMatch("ha僕niケーキwo", 1="ha", 2="僕niケーキ")
+
+julia> m1=match(r"(wo|ha|ni|ru)(.+)(wo|ha|ni|ru)",m3)
+RegexMatch("ha僕niケーキwo与えru", 1="ha", 2="僕niケーキwo与え", 3="ru")
+
+julia> m1.captures
+3-element Array{Union{Nothing, SubString{String}},1}:
+"ha"
+"僕niケーキwo与え"
+"ru"
+
+★★
+function parsej(m::String)
+ token = []
+ rjo = r"(.+)(wo|ha|ni|ru)"
+ pm  = m
+ itks = ""
+ pos =[]
+ while true
+   tks = match(rjo, pm)
+   if nothing == tks; break end
+   cm = tks.captures
+   push!(token, (cm[1], cm[2]))
+   pm = cm[1]
+ end
+ return token
+end
+
+m3="君ha僕niケーキwo与えru"
+julia> parsej(m3)
+4-element Array{Any,1}:
+("君ha僕niケーキwo与え", "ru")
+("君ha僕niケーキ", "wo")
+("君ha僕", "ni")
+("君", "ha")
+==#
+
